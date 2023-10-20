f42853e2bc0fb8dd96714529045644cf2f1b894d
Armaan339 <armaanbrar339@gmail.com>
2023-10-20 16:08:17 -0700

    project done

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom"
+import {Typography , Card,CardMedia,CardContent, } from "@mui/material"
+import { CheckCircle } from "@mui/icons-material"
+
+import { demoVideoTitle ,demoChannelTitle,demoChannelUrl,demoVideoUrl} from "../utils/constants"
+
+
+const VideoCard = ({video:{id:{videoId},snippet}}) => {
+  return (
+    <Card sx={{width:{xs:'100%',sm:'358px',md:'280px'},boxShadow:'none',borderRadius:0}}>
+        <Link to={videoId?`/video/${videoId}`:demoVideoUrl}>
+        <CardMedia image={snippet?.thumbnails?.high?.url} alt={snippet?.title}
+        sx={{width:{xs:'100%',sm:'358px',md:'280px'},height:180 }}/>
+        </Link>
+
+        <CardContent sx={{backgroundColor:'#1e1e1e',height:'106px'}}>
+            <Link to={videoId?`/video/${videoId}`:demoVideoUrl}>
+                <Typography variant='subtile1' fontWeight='bold' color='#FFF'>
+                    {snippet?.title.slice(0,60)||demoVideoTitle.slice(0,60)}
+                </Typography>
+            </Link>
+
+            <Link to={snippet?.channelId?`/channel/${snippet?.channelId}`:demoChannelUrl}>
+                <Typography variant='subtitle2' fontWeight='bold' color='#FFF'>
+                    {snippet?.channelTitle||demoChannelTitle}
+                    <CheckCircle sx={{fontSize:12,color:'gray',ml:'5px'}}/>
+                </Typography> 
+            </Link>
+
+        </CardContent>
+    </Card>
+  )
+}
+
+export default VideoCard
